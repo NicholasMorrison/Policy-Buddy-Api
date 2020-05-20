cf9120ee51216758e3e4dbd802b22a2dbbdf4133
NicholasMorrison <nmorrison096@gmail.com>
2020-05-20 02:02:04 -0400

    fix hello world example router

--- app/api/hello/router.py
+++ app/api/hello/router.py
@@ -1,9 +1,9 @@
 """ a declaration of url paths for the hello api """
 
 from flask import Blueprint
-import controller
+from app.api.hello import controller
 
-router = Blueprint('hello', __name__, url_prefix='/hello')
+router = Blueprint('hello', __name__)
 
-router.add_url_rule('/', view_func=controller.handle_index)
-router.add_url_rule('/<name>', view_func=controller.handle_name)
+router.add_url_rule('/hello', view_func=controller.handle_index)
+router.add_url_rule('/hello/<name>', view_func=controller.handle_name)

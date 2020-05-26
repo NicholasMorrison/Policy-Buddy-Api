b0508ef5ea7d7a38f6d7acba670ac5cafeb97e94
NicholasMorrison <nmorrison096@gmail.com>
2020-05-25 23:45:34 -0400

    add more validation tests for /tags/common endpoint

--- test/integration/tags/test_find_common.py
+++ test/integration/tags/test_find_common.py
@@ -6,9 +6,29 @@ key points that stand out in the terms agreement
 # pylint: disable=redefined-outer-name
 # pylint: disable=unused-import
 from test.resource.client import client
-ROUTE = '/test/common'
+ROUTE = '/tags/common'
 
 def test_post_method_enforced(client):
     """ test that get requests are rejected """
     response = client.get(ROUTE)
-    assert response.status_code == 404
+    assert response.status_code == 405
+
+def test_empty_body_rejected(client):
+    """ test that a request with an empty body is rejected w/ validation error """
+    response = client.post(ROUTE)
+    assert response.status_code == 422
+
+def test_body_content_string(client):
+    """ test the validation of the content parameter in the body """
+    response = client.post(ROUTE, json={
+        'content': 'i should not be a string'
+    })
+    assert response.status_code == 422
+
+def test_body_content_list(client):
+    """ test that a list associated to the content body parameter is processible """
+    response = client.post(ROUTE, json={
+        'content': ['first paragraph', 'second paragraph']
+    })
+    print(response.data)
+    assert response.status_code == 200
